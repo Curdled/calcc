ae3ca9b19292d090f8b0821b81b9f9d38fdd914e
Joe Isaacs <joe.isaacs@live.co.uk>
2015-08-12 19:05:12 +0100

    Updated the tests

--- src/test/scala/calcc/lexerTest.scala
+++ src/test/scala/calcc/lexerTest.scala
@@ -73,47 +73,96 @@ class lexerTest extends FunSuite with BeforeAndAfter {
 
 
   test("Parsing + - ! *") {
-    assert(l.parse("+") == List(Plus(0)))
-    assert(l.parse("+-!*++") == List(Plus(0), Minus(1), Fact(2), Multi(3), Plus(4), Plus(5)))
+    assert(l.parse("+") match{
+      case Left(v) => v == List(Plus(0))
+      case Right(_) => false
+    })
+
+    assert(l.parse("+-!*++") match{
+      case Left(v) =>  v == List(Plus(0), Minus(1), Fact(2), Multi(3), Plus(4), Plus(5))
+      case Right(_) => false
+    })
   }
 
   test("Parsing with spaces also") {
-    assert(l.parse(" + - ! * +") == List(WS(0), Plus(1), WS(2), Minus(3), WS(4), Fact(5), WS(6), Multi(7), WS(8), Plus(9)))
+    assert(l.parse(" + - ! * +") match{
+      case Left(v) => v == List(WS(0), Plus(1), WS(2), Minus(3), WS(4), Fact(5), WS(6), Multi(7), WS(8), Plus(9))
+      case Right(_) => false
+    })
   }
 
   test("Parsing with digits as well"){
-    assert(l.parse("2+1") == List(Num(List('2'), 0), Plus(1), Num(List('1'), 2)))
+    assert(l.parse("2+1") match{
+      case Left(v) =>  v == List(Num(List('2'), 0), Plus(1), Num(List('1'), 2))
+      case Right(_) => false
+    })
   }
 
   test("Parsing with numbers as well"){
-    assert(l.parse("19 ") == List(Num(List('1', '9'), 0), WS(2)))
-    assert(l.parse("-222-+12") == List(Minus(0), Num(List('2', '2', '2'), 1), Minus(4), Plus(5), Num(List('1','2'), 6)))
+    assert(l.parse("19 ") match{
+      case Left(v) =>  v == List(Num(List('1', '9'), 0), WS(2))
+      case Right(_) => false
+    })
+    assert(l.parse("-222-+12") match{
+      case Left(v) =>  v == List(Minus(0), Num(List('2', '2', '2'), 1), Minus(4), Plus(5), Num(List('1','2'), 6))
+      case Right(_) => false
+    })
   }
 
   test("Parsing unsigned floats") {
-    assert(l.parse("1.") == List(Num(List('1', '.'), 0)))
-    assert(l.parse("1.123") == List(Num(List('1', '.', '1', '2', '3'), 0)))
-    assert(l.parse("1.23+1.56345345") ==
-      List(Num(List('1', '.', '2', '3'),0), Plus(4), Num(List('1', '.', '5', '6', '3', '4', '5', '3', '4', '5'),5)))
+    assert(l.parse("1.") match{
+      case Left(v) =>  v == List(Num(List('1', '.'), 0))
+      case Right(_) => false
+    })
+    assert(l.parse("1.123") match {
+      case Left(v) => v == List(Num(List('1', '.', '1', '2', '3'), 0))
+      case Right(_) => false
+    })
+    assert(l.parse("1.23+1.56345345") match{
+      case Left(v) =>
+        v == List(Num(List('1', '.', '2', '3'),0), Plus(4), Num(List('1', '.', '5', '6', '3', '4', '5', '3', '4', '5'),5))
+      case Right(_) => false
+    })
   }
 
   test("Test with Exp") {
-    assert(l.parse("1E11") == List(Num(List('1','E','1','1'),0)))
+    assert(l.parse("1E11") match{
+      case Left(v) =>  v == List(Num(List('1','E','1','1'),0))
+      case Right(_) => false
+    })
   }
 
   test("Test with Exp and frac") {
-    assert(l.parse("1.4e1") == List(Num(List('1','.','4','e','1'),0)))
-    assert(l.parse("021.6E+1++") == List(Num(List('0', '2', '1', '.','6','E', '+', '1'),0), Plus(8), Plus(9)))
-    assert(l.parse("1.4E-1") == List(Num(List('1','.','4','E', '-', '1'),0)))
+    assert(l.parse("1.4e1") match{
+      case Left(v) =>  v == List(Num(List('1','.','4','e','1'),0))
+      case Right(_) => false
+    })
+    assert(l.parse("021.6E+1++") match{
+      case Left(v) =>  v == List(Num(List('0', '2', '1', '.','6','E', '+', '1'),0), Plus(8), Plus(9))
+      case Right(_) => false
+    })
+    assert(l.parse("1.4E-1") match{
+      case Left(v) =>  v == List(Num(List('1','.','4','E', '-', '1'),0))
+      case Right(_) => false
+    })
   }
 
   test("Test with cos") {
-    assert(l.parse("cos1") == List(Cos(0), Num(List('1'), 3)))
-    assert(l.parse("cOs!*1") == List(Cos(0), Fact(3), Multi(4), Num(List('1'), 5)))
+    assert(l.parse("cos1") match{
+      case Left(v) =>  v == List(Cos(0), Num(List('1'), 3))
+      case Right(_) => false
+    })
+    assert(l.parse("cOs!*1") match{
+      case Left(v) =>  v == List(Cos(0), Fact(3), Multi(4), Num(List('1'), 5))
+      case Right(_) => false
+    })
   }
 
   test("Random") {
-    assert(l.parse("co s+1.1E-1") == List(Cos(0), Plus(3), Num(List('1', '.', '1', 'E', '-', '1'),4)))
+    assert(l.parse("cos+1.1E-1%%") match{
+      case Left(_) => false
+      case Right(v) =>  v == "Value at position 11 not expected"
+    })
   }
 
 }
