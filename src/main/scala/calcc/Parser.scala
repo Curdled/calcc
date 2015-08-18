ae3478352c697e7571396fab73d0cdf38294ba5d
Curdled <joe.isaacs@live.co.uk>
2015-08-18 11:45:12 +0100

    Added a LR(0) parser with tree

--- src/main/scala/calcc/Parser.scala
+++ src/main/scala/calcc/Parser.scala
@@ -1,10 +1,13 @@
 package calcc
 
 
+import calcc.GrammarTable.LRTable
+
 
 /**
  * Created by joeisaacs on 16/08/15.
  */
+
 
 
 abstract class Action
@@ -14,4 +17,28 @@ case class Reduce(production: Production) extends Action
 object Accept extends Action{
   override def toString = "acc"
 }
-case class Error(state: Int) extends Action
+
+
+class Parser(table:  LRTable){
+  def parse(input: List[Terminal]) = {
+    def parser(input: List[Terminal], stack: List[Int],t: List[Tree[GrammarSymbol]]): Tree[GrammarSymbol] = {
+      table._1.get((input.head,stack.head)) match{
+        case None => throw new IllegalArgumentException(s"No next state in the action table")// ${input.head.pos}")
+        case Some(x) => x match{
+          case Shift(y) => parser(input.tail, y :: stack, Node(input.head, List(Leaf)) :: t)
+          case Reduce(y) =>
+            val newStack = stack.drop(y.right.size)
+            table._2.get((y.left, newStack.head)) match{
+              case None => throw new IllegalArgumentException(s"No next state in the GOTO table")
+              case Some(z) =>
+                val (treeHead, treeTail) = t.splitAt(y.right.size)
+                parser(input, z::newStack, Node[GrammarSymbol](y.left,treeHead.reverse) :: treeTail)
+            }
+          case Accept => t.head
+        }
+      }
+    }
+    parser(input, List(0), List(Leaf))//add the start state
+  }
+
+}
---
+++ src/main/scala/calcc/Tree.scala
@@ -0,0 +1,13 @@
+package calcc
+
+/**
+ * Created by joeisaacs on 18/08/15.
+ */
+abstract class Tree[+A]
+
+case class Node[+A](h: A, t: List[Tree[A]]) extends Tree[A]{
+}
+
+object Leaf extends Tree{
+  override def toString = ""
+}
