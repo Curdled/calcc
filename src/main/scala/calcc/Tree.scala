435d7c124292774b42608895f04679fc7fecef56
Curdled <joe.isaacs@live.co.uk>
2015-08-19 16:06:05 +0100

    Added printing code

--- src/main/scala/calcc/Tree.scala
+++ src/main/scala/calcc/Tree.scala
@@ -1,12 +1,25 @@
 package calcc
 
-/**
- * Created by joeisaacs on 18/08/15.
- */
-abstract class Tree[+A]
 
-case class Node[+A](h: A, t: List[Tree[A]]) extends Tree[A]
+abstract class Tree[+A]{
+  def print(indent: Int): String
+}
+
+case class Node[+A](h: A, t: List[Tree[A]]) extends Tree[A]{
+  def wsIndent(i:Int): String ={
+    if (i <= 0) ""
+    else "-" + wsIndent(i-1)
+  }
+  def print(indent: Int): String ={
+    wsIndent(indent) + h + "\n" + t.foldLeft("")((acc, tree) => acc + tree.print(indent+1))
+  }
+  override def toString = {
+    print(0)
+  }
+}
 
 object Leaf extends Tree{
-  override def toString = "Leaf"
+  override def toString = ""
+
+  override def print(indent: Int): String = ""
 }
